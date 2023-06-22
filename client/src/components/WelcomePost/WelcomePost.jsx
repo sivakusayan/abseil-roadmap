9c6f51031d8dc0ea5bfd6af81a8c0d8e5f8bfc7f
Sayan Sivakumaran <sivakusayan@gmail.com>
2023-06-21 21:30:06 -0500

    Add better colors

--- client/src/components/WelcomePost/WelcomePost.jsx
+++ client/src/components/WelcomePost/WelcomePost.jsx
@@ -3,7 +3,7 @@ import { Fragment } from "preact";
 export default function WelcomePost() {
     return (
         <Fragment>
-            <p>Hello! This is a frontend wrapper around <a href="https://abseil.io/tips/">Abseil's C++ tips of the week</a>.</p>
+            <h1>Hello! This is a frontend wrapper around <a href="https://abseil.io/tips/">Abseil's C++ tips of the week</a>.</h1>
             <p>This website should be much lighter, and allows you to mark what tips you have or haven't read.</p>
             <p>If you really want to, you can also create an account to sync your read tips across devices, as well as configure email reminders to read a tip you haven't read yet.</p>
         </Fragment>
